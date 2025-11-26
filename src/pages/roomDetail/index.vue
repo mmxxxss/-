51e6e62bf48be79950c900a89e93b3aa25150213
马晓帅 <maxs3@yonyou.com>
2025-11-26 15:42:47 +0800

    feat:收藏

--- src/pages/roomDetail/index.vue
+++ src/pages/roomDetail/index.vue
@@ -25,6 +25,7 @@ const getRoomDetailData = async () => {
 getRoomDetailData();
 // 获取是否收藏
 const isCollect = ref(false);
+const collectInfo = ref({});
 const getIsCollectData = async () => {
   let id = Taro.getCurrentInstance().router?.params?.id;
   let userid = Taro.getStorageSync("userid");
@@ -38,10 +39,50 @@ const getIsCollectData = async () => {
   };
   const res = await getIsCollect(param);
   if (res.code == 0) {
-    isCollect.value = res.data.length > 0;
+    isCollect.value = res.data.list.length > 0;
+    collectInfo.value = res.data.list[0];
   }
 };
 getIsCollectData();
+// 收藏房源
+const addCollectData = async () => {
+  let id = Taro.getCurrentInstance().router?.params?.id;
+  let userid = Taro.getStorageSync("userid");
+  let param = {
+    name: roomDetail.value.fangwumingcheng,
+    picture: roomDetail.value.fangwutupian.split(",")[0],
+    refId: id,
+    tableName: "fangyuanxinxi",
+    userid: userid,
+    type: 1,
+  };
+  const res = await addCollect(param);
+  if (res.code == 0) {
+    Taro.showToast({
+      title: "收藏成功",
+      icon: "none",
+    });
+    roomDetail.value.storeupNumber += 1;
+    await updateCollect(roomDetail.value);
+    getRoomDetailData();
+    getIsCollectData();
+  }
+};
+// 取消收藏房源
+const delCollectData = async () => {
+  let param = [collectInfo.value.id];
+  const res = await delCollect(param);
+  if (res.code == 0) {
+    Taro.showToast({
+      title: "取消收藏成功",
+      icon: "none",
+    });
+    roomDetail.value.storeupNumber -= 1;
+    await updateCollect(roomDetail.value);
+    getRoomDetailData();
+    getIsCollectData();
+  }
+};
 </script>
 <template>
   <div>
@@ -69,13 +110,119 @@ getIsCollectData();
     <div class="content">
       <div class="top">
         <h1>{{ roomDetail.fangyuanbianhao }}</h1>
-        <div v-if="!isCollect">
+        <div v-if="!isCollect" class="collect" @click="addCollectData">
           <span>收藏</span
-          ><Image src="../../assets/cancelLike.png" alt="" class="img" />
+          ><image src="../../assets/cancelLike.png" alt="" class="img" />
         </div>
-        <div v-else>
+        <div v-else class="collect" @click="delCollectData">
           <span>取消收藏</span
-          ><Image src="../../assets/like.png" alt="" class="img" />
+          ><image src="../../assets/like.png" alt="" class="img" />
+        </div>
+      </div>
+      <div class="content-item">
+        <div class="content-item-left">
+          <div>房屋名称</div>
+          <div class="content-item-left-text">
+            {{ roomDetail.fangwumingcheng }}
+          </div>
+        </div>
+        <div class="content-item-right">
+          <div>房屋面积</div>
+          <div class="content-item-right-text">
+            {{ roomDetail.fangwumianji }}
+          </div>
+        </div>
+      </div>
+      <div class="content-item">
+        <div class="content-item-left">
+          <div>发布时间</div>
+          <div class="content-item-left-text">
+            {{ roomDetail.fabushijian }}
+          </div>
+        </div>
+        <div class="content-item-right">
+          <div>房屋地址</div>
+          <div class="content-item-right-text">
+            {{ roomDetail.fangwudizhi }}
+          </div>
+        </div>
+      </div>
+      <div class="content-item">
+        <div class="content-item-left">
+          <div>租赁方式</div>
+          <div class="content-item-left-text">
+            {{ roomDetail.zulinfangshi }}
+          </div>
+        </div>
+        <div class="content-item-right">
+          <div>租赁价格</div>
+          <div class="content-item-right-text">
+            {{ roomDetail.zulinjiage }}
+          </div>
+        </div>
+      </div>
+      <div class="content-item">
+        <div class="content-item-left">
+          <div>是否有人租赁</div>
+          <div class="content-item-left-text">
+            {{ roomDetail.shifouyourenzulin }}
+          </div>
+        </div>
+        <div class="content-item-right">
+          <div>房东</div>
+          <div class="content-item-right-text">
+            {{ roomDetail.fangdong }}
+          </div>
+        </div>
+      </div>
+      <div class="content-item">
+        <div class="content-item-left">
+          <div>房东姓名</div>
+          <div class="content-item-left-text">
+            {{ roomDetail.fangdongxingming }}
+          </div>
+        </div>
+        <div class="content-item-right">
+          <div>手机号</div>
+          <div class="content-item-right-text">
+            {{ roomDetail.shoujihao }}
+          </div>
+        </div>
+      </div>
+      <div class="content-item">
+        <div class="content-item-left">
+          <div>邮箱</div>
+          <div class="content-item-left-text">
+            {{ roomDetail.youxiang }}
+          </div>
+        </div>
+        <div class="content-item-right">
+          <div>收藏数</div>
+          <div class="content-item-right-text">
+            {{ roomDetail.storeupNumber }}
+          </div>
+        </div>
+      </div>
+      <div class="content-item">
+        <div class="content-item-left">
+          <div>评论数</div>
+          <div class="content-item-left-text">
+            {{ roomDetail.discussNumber }}
+          </div>
+        </div>
+        <div class="content-item-right">
+          <div>地址</div>
+          <div class="content-item-right-text">
+            {{ roomDetail.fulladdress }}
+          </div>
+        </div>
+      </div>
+      <div class="content-item">
+        <div class="content-item-left">
+          <div>租赁人数</div>
+          <div class="content-item-left-text">
+            {{ roomDetail.zulinrenshu }}
+          </div>
         </div>
       </div>
     </div>
@@ -92,12 +239,43 @@ getIsCollectData();
   flex-direction: column;
   .top {
     display: flex;
+    justify-content: space-between;
+    align-items: center;
     h1 {
       font-size: 36px;
     }
-    .img {
-      width: 40px;
-      height: 40px;
+    .collect {
+      display: flex;
+      align-items: center;
+      span {
+        font-size: 28px;
+        color: #8a8a8a;
+      }
+      .img {
+        width: 32px;
+        height: 32px;
+        margin-left: 5px;
+      }
+    }
+  }
+  .content-item {
+    display: flex;
+    align-items: center;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    &-left {
+      width: 350px;
+      &-text {
+        font-size: 28px;
+        color: #8a8a8a;
+      }
+    }
+    &-right {
+      margin-left: 20px;
+      &-text {
+        font-size: 28px;
+        color: #8a8a8a;
+      }
     }
   }
 }
